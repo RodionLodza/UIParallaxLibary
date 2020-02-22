dc33c01afbadd86bebd08fbfa8a34e533740d0a8
Rodion Lodza <r.lodza@gmail.com>
2020-02-22 23:34:47 +0700

    Moved libary to repository

---
+++ Assets/Example/Scripts/UIParallaxManagerExample.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UIParallax;
+
+[RequireComponent(typeof(UIParallaxEffect))]
+public class UIParallaxManagerExample : MonoBehaviour
+{
+    [SerializeField] private UIParallaxEffect parallaxEffect;
+
+    private void Start()
+    {
+        if (parallaxEffect == null)
+            parallaxEffect = GetComponent<UIParallaxEffect>();
+
+        parallaxEffect.Initialize();
+    }
+
+    private void Update()
+    {
+        parallaxEffect.RefreshParallaxEffect();
+    }
+}
