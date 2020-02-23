7cb5af69699ece97aa563ef204c14a28c66433ea
Rodion Lodza <r.lodza@gmail.com>
2020-02-23 11:47:48 +0700

    Setuped package branch

---
+++ Scripts/UIParallaxLayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+
+namespace UIParallax
+{
+    [Serializable]
+    public class UIParallaxLayer
+    {
+        public string ParallaxLayerName;
+        public RectTransform[] ObjectsRect;
+        public float PosXMultiplier;
+        public float PosYMultiplier;
+        public float RotationMultiplier;
+        public float InterpolationSpeed;
+    }
+}
